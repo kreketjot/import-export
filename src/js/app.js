9a04c33a1307ad16b91553c1d316d419bacc174c
Andrey Vinokurov <kreketjot@gmail.com>
2021-07-16 15:38:56 +0300

    ADDED webpack, eslint, babel and code with modules

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// TODO: write your code here
+import sum from './basic';
+import Game, {
+  GameSavingData,
+  readGameSaving as loadGame,
+  writeGameSaving as saveGame,
+} from './game';
+
+console.log('worked');
+
+console.log(sum([1, 2]));
+
+const game = new Game();
+game.start();
---
+++ src/js/game.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-classes-per-file */
+// eslint-disable-next-line no-unused-vars
+import Character from './domain';
+
+export default class Game {
+  // eslint-disable-next-line class-methods-use-this
+  start() {
+    // eslint-disable-next-line no-console
+    console.log('game started');
+  }
+}
+
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
